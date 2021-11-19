dea66b76cf1667c20a0d5a827e342dfae4e42ac9
geraldakbar <mgeraldakbar@gmail.com>
2021-11-20 00:41:26 +0700

    debug start.

--- main.pl
+++ main.pl
@@ -1,15 +1,29 @@
 /* FACTS */
-:- dynamic(role/2).
+:- dynamic(role/1).
+:- dynamic(exp/2).
+:- dynamic(level/2).
+:- dynamic(gold/1).
 
+level(player,1).
+level(farming,1).
+level(fishing,1).
+level(ranching,1).
+
+exp(farming,0).
+exp(ranching,0).
+exp(fishing,0).
+exp(player,0).
+
+gold(0).
 
 /*  RULES */
 /* startGame */
 startGame:-
     write(' _   _                           _'),nl,   
     write('| | | | __ _ _ ____   _____  ___| |_ '),nl,
-    write('| |_| |/ _` | \''),write('__\\ \\ / / _ \\/ __| __|'),nl,
-    write('|  _  | (_| | |  \\ V /  __/\\__ \\ |_ '),nl,
-    write('|_| |_|\\__,_|_|   \\_/ \\___||___/\\__|'),nl,nl,
+    write('| |_| |/ _` | \'__\\ \\ / / _ \\/ __| __|'),nl,
+    write('|  _  | (_| | |   \\ V /  __/\\__ \\ |_ '),nl,
+    write('|_| |_|\\__,_|_|    \\_/ \\___||___/\\__|'),nl,nl,
     write('Harvest Star!!!'),nl,nl,
     write('Let\'s play and pay our debts together!'),nl,nl,
     write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'),nl,
@@ -36,23 +50,32 @@ start:-
 
 /* writeRole buat nulis di start dan update role user */
 writeRole(1) :- 
-    write('Fisherman '),retractall(role(_,_)),assertz(role(user,_Fisherman)).
+    write('Fisherman'),asserta(role('Fisherman')),!.
 writeRole(2) :- 
-    write('Farmer'),retractall(role(_,_)),assertz(role(user,_Farmer)).
+    write('Farmer'),asserta(role('Farmer')),!.
 writeRole(3) :- 
-    write('Rancher'),retractall(role(_,_)),assertz(role(user,_Rancher)).
+    write('Rancher'),asserta(role('Rancher')),!.
 
 /* status */
-/* belom bener harus masukin variabelnya bingung*/
 status :-
     write('Your status: '),nl,
-    write('Job: '),writeRole(1),nl,
-    write('Level: '),nl,
-    write('Level farming; '),nl,
-    write('Exp farming: '),nl,
-    write('Level fishing; '),nl,
-    write('Exp fishing: '),nl,
-    write('Level ranching; '),nl,
-    write('Exp ranching: '),nl,
-    write('Exp: '),nl,
-    write('Gold: '),nl.
+    role(Y),!,
+    write('Job: '),write(Y),nl,
+    level(player,Player_Level),!,
+    write('Level: '),write(Player_Level),nl,
+    level(farming,Farming_Level),
+    write('Level farming: '),write(Farming_Level),nl,
+    exp(farming,Farming_EXP),!,
+    write('Exp farming: '),write(Farming_EXP),nl,
+    level(fishing,Fishing_Level),
+    write('Level fishing; '),write(Fishing_Level),nl,
+    exp(fishing,Fishing_EXP),!,
+    write('Exp fishing: '),write(Fishing_EXP),nl,
+    level(ranching,Ranching_Level),
+    write('Level ranching; '),write(Ranching_Level),nl,
+    exp(ranching,Ranching_EXP),!,
+    write('Exp ranching: '),write(Ranching_EXP),nl,
+    exp(player,Player_EXP),!,
+    write('Exp: '),write(Player_EXP),nl,
+    gold(GOLD),!,
+    write('Gold: '),write(GOLD),nl.
