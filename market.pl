f0cfb687ec3a5f4da8757eaf187809c7042c043b
raniadf <16520006@mahasiswa.itb.ac.id>
2021-11-22 13:08:35 +0700

    add market and items

--- market.pl
+++ market.pl
@@ -1,5 +1,7 @@
 /* FACTS */
+:- include(items.pl).
 :- dynamic(inMarket/0).
+
 
 /* RULES */
 
@@ -11,4 +13,15 @@ market :-
 
 /* Entering the market */
 /*assertz(inMarket), --> janlup tambahin ini pi klo dah ada posisi playernya*/
-market :- write_ln('What do you want to do?'), write_ln('1. Buy'), write_ln('2.Sell').
+market :- write_ln('What do you want to do?'), 
+        write_ln('1. Buy'), 
+        write_ln('2.Sell'),
+        read(X), (X =:= 1 -> buy; X =:= 2 -> sell), nl, !.
+market :- 
+
+/* Buy items */
+
+
+/* Exit market */
+exitMarket :- \+inMarket, nl, write_ln('Anda belum berada di market.').
+exitMarket :- inMarket, nl, write_ln('Terima kasih sudah berkunjung ke market! Sampai jumpa kembali!').
