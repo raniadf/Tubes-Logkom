3a9e745fcf5bad22dd36efa957cdbdbd94ed5e0f
raniadf <16520006@mahasiswa.itb.ac.id>
2021-11-25 20:05:20 +0700

    add and change some items

--- items.pl
+++ items.pl
@@ -1,23 +1,39 @@
-/** FACTS **/
+/*** FACTS ***/
 
-/* Farming */
-item(seed, carrot, 1).
-item(seed, sweet_potato, 2).
-item(seed, cassava, 3).
-item(seed, corn, 4).
-item(seed, tomato, 5).
-item(seed, potato, 6).
+/* item(itemname, id, price,  min farming level, min fishing level, min farming level)
+equipment(equipmentname, id, price, min farming level, min fishing level, min farming level) */
 
-/* Fishing */
-item(food, grade_a, 7).
-item(food, grade_b, 8).
-item(food, grade_c, 9).
+/** Farming **/
+/* Item */
+item(carrot_seed, 1, 50, 1, 0, 0).
+item(sweet_potato_seed, 2, 80, 1, 0, 0).
+item(cassava_seed, 3, 120, 1, 0, 0).
+item(corn_seed, 4, 140, 2, 0, 0).
+item(tomato_seed, 5, 130, 3, 0, 0).
+item(potato_seed, 6, 150, 4, 0, 0).
+/* Equipment */
+equipment(shovel, 1, 250, 3, 0, 0).
+equipment(hand_fork, 2, 150, 1, 0, 0).
+equipment(watering_can, 3, 300, 1, 0, 0).
 
-/* Farming */
-item(food, chicken, 10).
-item(food, cow, 11).
-item(food, sheep, 12).
 
+/** Fishing **/
+/* Item */
+item(grade_a_food, 7, 300, 0, 4, 0).
+item(grade_b_food, 8, 200, 0, 2, 0).
+item(grade_c_food, 9, 100, 0, 1, 0).
+/* Equipment */
+equipment(fishnet, 4, 250, 0, 3, 0).
+equipment(rod, 5, 100, 0, 1, 0).
+
+/** Farming **/
+/* Item */
+item(chicken_food, 10, 100, 0, 0, 1).
+item(cow_food, 11, 125, 0, 0, 1).
+item(sheep_food, 12, 170, 0, 0, 1).
+/* Equipment */
+equipment(milk_pail, 6, 100, 0, 0, 1).
+equipment(shears, 7, 150, 0, 0, 1).
 
 /* RULES */
 
