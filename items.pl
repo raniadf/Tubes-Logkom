6f8b47a38b4e7400c8634633e5dd09f952b875f0
raniadf <16520006@mahasiswa.itb.ac.id>
2021-11-27 18:04:09 +0700

    change equipment to items

--- items.pl
+++ items.pl
@@ -1,7 +1,6 @@
 /*** FACTS ***/
 
-/* item(itemname, id, price,  min farming level, min fishing level, min farming level)
-equipment(equipmentname, id, price, min farming level, min fishing level, min farming level) */
+/* item(itemname, id, price,  min farming level, min fishing level, min farming level) */
 
 /** Farming **/
 /* Item */
@@ -12,10 +11,9 @@ item(corn_seed, 4, 140, 2, 0, 0).
 item(tomato_seed, 5, 130, 3, 0, 0).
 item(potato_seed, 6, 150, 4, 0, 0).
 /* Equipment */
-equipment(shovel, 1, 250, 3, 0, 0).
-equipment(hand_fork, 2, 150, 1, 0, 0).
-equipment(watering_can, 3, 300, 1, 0, 0).
-
+item(shovel, 1, 250, 3, 0, 0).
+item(hand_fork, 2, 150, 1, 0, 0).
+item(watering_can, 3, 300, 1, 0, 0).
 
 /** Fishing **/
 /* Item */
@@ -23,8 +21,8 @@ item(grade_a_food, 7, 300, 0, 4, 0).
 item(grade_b_food, 8, 200, 0, 2, 0).
 item(grade_c_food, 9, 100, 0, 1, 0).
 /* Equipment */
-equipment(fishnet, 4, 250, 0, 3, 0).
-equipment(rod, 5, 100, 0, 1, 0).
+item(fishnet, 4, 250, 0, 3, 0).
+item(rod, 5, 100, 0, 1, 0).
 
 /** Farming **/
 /* Item */
@@ -32,8 +30,8 @@ item(chicken_food, 10, 100, 0, 0, 1).
 item(cow_food, 11, 125, 0, 0, 1).
 item(sheep_food, 12, 170, 0, 0, 1).
 /* Equipment */
-equipment(milk_pail, 6, 100, 0, 0, 1).
-equipment(shears, 7, 150, 0, 0, 1).
+item(milk_pail, 6, 100, 0, 0, 1).
+item(shears, 7, 150, 0, 0, 1).
 
 /* RULES */
 
