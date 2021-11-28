f79e1e351f1ce9c2f8e86e21e24aea1b767df010
raniadf <70957597+raniadf@users.noreply.github.com>
2021-11-29 00:38:29 +0700

    change format

--- items.pl
+++ items.pl
@@ -1,6 +1,6 @@
 /*** FACTS ***/
 
-/* item(itemname, id, price,  min farming level, min fishing level, min farming level) */
+/* item(itemname, qty, price, min farming level, min fishing level, min ranching level) */
 
 /** Farming **/
 /* Farm product */
@@ -11,16 +11,16 @@ item(corn, 0, 0, 0, 0, 0).
 item(tomato, 0, 0, 0, 0, 0).
 item(potato, 0, 0, 0, 0, 0).
 /* Item */
-item(carrot_seed, 1, 50, 1, 0, 0).
-item(sweet_potato_seed, 2, 80, 1, 0, 0).
-item(cassava_seed, 3, 120, 1, 0, 0).
-item(corn_seed, 4, 140, 2, 0, 0).
-item(tomato_seed, 5, 130, 3, 0, 0).
-item(potato_seed, 6, 150, 4, 0, 0).
+item(carrot_seed, 0, 50, 1, 0, 0).
+item(sweet_potato_seed, 0, 80, 1, 0, 0).
+item(cassava_seed, 0, 120, 1, 0, 0).
+item(corn_seed, 0, 140, 2, 0, 0).
+item(tomato_seed, 0, 130, 3, 0, 0).
+item(potato_seed, 0, 150, 4, 0, 0).
 /* Equipment */
-item(shovel, 1, 250, 3, 0, 0).
-item(hand_fork, 2, 150, 1, 0, 0).
-item(watering_can, 3, 300, 1, 0, 0).
+item(shovel, 0, 250, 3, 0, 0).
+item(hand_fork, 0, 150, 1, 0, 0).
+item(watering_can, 0, 300, 1, 0, 0).
 
 /** Fishing **/
 /* Fish product */
@@ -30,25 +30,24 @@ item(mahi_mahi, 0, 0, 0, 0, 0).
 item(red_snapper, 0, 0, 0, 0, 0).
 item(catfish, 0, 0, 0, 0, 0).
 /* Item */
-item(grade_a_food, 7, 300, 0, 4, 0).
-item(grade_b_food, 8, 200, 0, 2, 0).
-item(grade_c_food, 9, 100, 0, 1, 0).
+item(grade_a_food, 0, 300, 0, 4, 0).
+item(grade_b_food, 0, 200, 0, 2, 0).
+item(grade_c_food, 0, 100, 0, 1, 0).
 /* Equipment */
-item(fishnet, 4, 250, 0, 3, 0).
-item(rod, 5, 100, 0, 1, 0).
+item(fishnet, 0, 250, 0, 3, 0).
+item(rod, 0, 100, 0, 1, 0).
 
 /** Farming **/
 /* Farm product */
-item(chicken_egg, 13, 0, 0, 0).
-item(milk, 14, 0, 0, 0, 0).
-item(wool, 15, 0, 0, 0, 0).
+item(chicken_egg, 0, 0, 0, 0).
+item(milk, 0, 0, 0, 0, 0).
+item(wool, 0, 0, 0, 0, 0).
 /* Item */
-item(chicken, 10, 500, 0, 0, 1).
-item(cow, 11, 1000, 0, 0, 1).
-item(sheep, 12, 1500, 0, 0, 1).
+item(chicken, 0, 500, 0, 0, 1).
+item(cow, 0, 1000, 0, 0, 1).
+item(sheep, 0, 1500, 0, 0, 1).
 /* Equipment */
-item(milk_pail, 6, 100, 0, 0, 1).
-item(shears, 7, 150, 0, 0, 1).
+item(milk_pail, 0, 100, 0, 0, 1).
+item(shears, 0, 150, 0, 0, 1).
 
 /* RULES */
-
