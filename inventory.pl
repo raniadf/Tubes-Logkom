8e918220f79b955492da6e1e64db4be012e17923
raniadf <16520006@mahasiswa.itb.ac.id>
2021-11-29 08:15:30 +0700

    modify

--- inventory.pl
+++ inventory.pl
@@ -55,11 +55,11 @@ makeListItems(ListofLevel, ListofQty, ListofName) :-
 displayInventory([], [], []).
 displayInventory([A|X], [B|Y], [C|Z]) :-
     A >= 1,
-    write(B), write(' Level '), write(A), write(' '), printItemName(C), nl,
+    write(B), write(' Level '), write(A), write(' '), printItem(C), nl,
     displayInventory(X, Y, Z).
 displayInventory([A|X], [B|Y], [C|Z]) :-
     A < 1,
-    write(B), write(' '), printItemName(C), nl,
+    write(B), write(' '), printItem(C), nl,
     displayInventory(X, Y, Z).
 
 
@@ -73,7 +73,7 @@ addItem(ItemName, Quantity) :-
     item(ItemName, Qty, Price, Level, FarmLevel, FishLevel, RanchLevel),
     Qty2 is Qty + Quantity,
     asserta(inventory(ItemName, Qty2, Price, Level, FarmLevel, FishLevel, RanchLevel)),
-    write('You got '), write(Quantity), write(' '), printItemName(ItemName), write('!'), nl, !.
+    write('You got '), write(Quantity), write(' '), printItem(ItemName), write('!'), nl, !.
 
 /* Case 2: Sudah ada item 'itemName' di Inventory */
 addItem(ItemName, Quantity) :-
@@ -84,7 +84,7 @@ addItem(ItemName, Quantity) :-
     Qty2 is Qty + Quantity,
     retract(inventory(ItemName, Qty, Price, Level, FarmLevel, FishLevel, RanchLevel)),
     asserta(inventory(ItemName, Qty2, Price, Level, FarmLevel, FishLevel, RanchLevel)),
-    write('You got '), write(Quantity), write(' '), printItemName(ItemName), write('!'), nl, !.
+    write('You got '), write(Quantity), write(' '), printItem(ItemName), write('!'), nl, !.
 
 /* Case 3: Inventory penuh sebelum melakukan addItem */
 addItem(_, _) :-
@@ -103,231 +103,28 @@ addItem(ItemName, Quantity) :-
     Qty2 is Qty + QtyAvailable,
     retract(inventory(ItemName, Qty, Price, Level, FarmLevel, FishLevel, RanchLevel)),
     asserta(inventory(ItemName, Qty2, Price, Level, FarmLevel, FishLevel, RanchLevel)),
-    write('Your inventory is full! Only '), write(QtyAvailable), write(' '), printItemName(ItemName), write(' can be added into your inventory!'), !.
+    write('Your inventory is full! Only '), write(QtyAvailable), write(' '), printItem(ItemName), write(' can be added into your inventory!'), !.
 
-
-/* *** REMOVE ITEMS *** */
-/* Dengan catatan, item yang ingin di-remove harus mencukupi */
-/* Case 1: Sudah ada item 'itemName' di inventory, jumlah mencukupi, dan jumlah item-nya tidak menjadi 0 setelah di-remove */
-removeItem(ItemName, Quantity) :-
-    inventory(ItemName, Qty, _, _, _, _, _),
-    Qty2 is Qty - Quantity,
-    Qty2 > 0,
-    retract(inventory(ItemName, Qty, Price, Level, FarmLevel, FishLevel, RanchLevel)),
-    asserta(inventory(ItemName, Qty2, Price, Level, FarmLevel, FishLevel, RanchLevel)), !.
-
-/* Case 2: Sudah ada item 'itemName' di inventory, jumlah mencukupi, dan jumlah item-nya menjadi 0 setelah di-remove */
-removeItem(ItemName, Quantity) :-
-    inventory(ItemName, Qty, _, _, _, _, _),
-    Qty2 is Qty - Quantity,
-    Qty2 =:= 0,
-    retract(inventory(ItemName, Qty, _, _, _, _, _)), !.
-
-
-/* *** THROW ITEMS *** */
-/* throwItem */
-throwItem :-
-    write('Your inventory'), nl,
-    makeListItems(ListofLevel, ListofQty, ListofName),
-    displayInventory(ListofLevel, ListofQty, ListofName),
-    nl,
-    write('What do you want to throw?'), nl,
-    read(ItemName),
-    throwItemName(ItemName), !.
-
-/* throwItemName */
-/* Case 1: Tidak ada item yang ingin di-throw */
-throwItemName(ItemName) :-
-    \+ inventory(ItemName, _, _, _, _, _, _),
-    nl,
-    write('You don\'t have '), printItemName(ItemName), write(' in your inventory! Cancelling...'), nl, !.
-/* Case 2: Terdapat item yang ingin di-throw */
-throwItemName(ItemName) :-
-    inventory(ItemName, Qty, _, _, _, _, _),
-    nl,
-    write('You have '), write(Qty), write(' '), printItemName(ItemName), write('. How many '), printItemName(ItemName), write(' do you want to throw? (type \'0\' if u want to cancel)'), nl,
-    read(Amount),
-    throwAmount(ItemName, Amount).
-
-/* throwAmount */
-/* Case 1: Pemain membatalkan proses throw */
-throwAmount(ItemName, Qty) :-
-    Qty =:= 0,
-    nl,
-    write('You cancelled to throw '), printItemName(ItemName), write('! Cancelling...'), nl, !.
-/* Case 2: Jumlah yang ingin di-throw melebihi stok di inventory */
-throwAmount(ItemName, ItemQty) :-
+/* *** DROP ITEMS *** */
+/* Drop Items */
+/* Case 1: Jumlah yang ingin di-drop melebihi stok di inventory */
+dropItems(ItemName, ItemQty) :-
     inventory(ItemName, Qty, _, _, _, _, _),
     ItemQty > Qty,
     nl,
-    write('You don\'t have enough '), printItemName(ItemName), write(' to throw! Cancelling...'), nl, !.
-/* Case 3: Jumlah yang ingin di-throw mencukupi */
-throwAmount(ItemName, ItemQty) :-
+    write('You don\'t have enough '), printItem(ItemName), write('... Sorry...'), nl, !.
+/* Case 2: Jumlah yang ingin di drop = jumlah quantity */
+dropItems(ItemName, ItemQty) :-
+    inventory(ItemName, Qty, _, _, _, _, _),
+    Qty2 is Qty - ItemQty,
+    Qty2 =:= 0,
+    retract(inventory(ItemName, Qty, _, _, _, _, _)), !.
+/* Case 3: Jumlah yang ingin di-drop mencukupi */
+dropItems(ItemName, ItemQty) :-
     inventory(ItemName, Qty, _, _, _, _, _),
     ItemQty =< Qty,
-    removeItem(ItemName, ItemQty),
+    Qty2 is Qty - ItemQty,
+    retract(inventory(ItemName, Qty, Price, Level, FarmLevel, FishLevel, RanchLevel)),
+    asserta(inventory(ItemName, Qty2, Price, Level, FarmLevel, FishLevel, RanchLevel)),
     nl,
-    write('You threw '), write(Qty), write(' '), printItemName(ItemName), write(' away!'), nl, !.
-
-
-/* *** PRINT ITEM *** */
-printItemName(ItemName) :-
-    ItemName == carrot,
-    write('Carrot'), !
-.
-printItemName(ItemName) :-
-    ItemName == sweet_potato,
-    write('Sweet Potato'), !
-.
-printItemName(ItemName) :-
-    ItemName == cassava,
-    write('Cassava'), !
-.
-printItemName(ItemName) :-
-    ItemName == corn,
-    write('Corn'), !
-.
-printItemName(ItemName) :-
-    ItemName == tomato,
-    write('Tomato'), !
-.
-printItemName(ItemName) :-
-    ItemName == potato,
-    write('Potato'), !
-.
-printItemName(ItemName) :-
-    ItemName == carrot_seed,
-    write('Carrot Seed'), !
-.
-printItemName(ItemName) :-
-    ItemName == sweet_potato_seed,
-    write('Sweet Potato Seed'), !
-.
-printItemName(ItemName) :-
-    ItemName == cassava_seed,
-    write('Cassava Seed'), !
-.
-printItemName(ItemName) :-
-    ItemName == corn_seed,
-    write('Corn Seed'), !
-.
-printItemName(ItemName) :-
-    ItemName == tomato_seed,
-    write('Tomato Seed'), !
-.
-printItemName(ItemName) :-
-    ItemName == potato_seed,
-    write('Potato Seed'), !
-.
-printItemName(ItemName) :-
-    ItemName == shovel_1,
-    write('Shovel'), !
-.
-printItemName(ItemName) :-
-    ItemName == shovel_2,
-    write('Shovel'), !
-.
-printItemName(ItemName) :-
-    ItemName == hand_fork_1,
-    write('Hand Fork'), !
-.
-printItemName(ItemName) :-
-    ItemName == hand_fork_2,
-    write('Hand Fork'), !
-.
-printItemName(ItemName) :-
-    ItemName == watering_can_1,
-    write('Watering Can'), !
-.
-printItemName(ItemName) :-
-    ItemName == watering_can_2,
-    write('Watering Can'), !
-.
-printItemName(ItemName) :-
-    ItemName == salmon,
-    write('Salmon'), !
-.
-printItemName(ItemName) :-
-    ItemName == tuna,
-    write('Tuna'), !
-.
-printItemName(ItemName) :-
-    ItemName == mahi_mahi,
-    write('Mahi-Mahi'), !
-.
-printItemName(ItemName) :-
-    ItemName == red_snapper,
-    write('Red Snapper'), !
-.
-printItemName(ItemName) :-
-    ItemName == catfish,
-    write('Catfish'), !
-.
-printItemName(ItemName) :-
-    ItemName == grade_a_food,
-    write('Grade A Food'), !
-.
-printItemName(ItemName) :-
-    ItemName == grade_b_food,
-    write('Grade B Food'), !
-.
-printItemName(ItemName) :-
-    ItemName == grade_c_food,
-    write('Grade C Food'), !
-.
-printItemName(ItemName) :-
-    ItemName == fishnet_1,
-    write('Fishnet'), !
-.
-printItemName(ItemName) :-
-    ItemName == fishnet_2,
-    write('Fishnet'), !
-.
-printItemName(ItemName) :-
-    ItemName == rod_1,
-    write('Fishing Rod'), !
-.
-printItemName(ItemName) :-
-    ItemName == rod_2,
-    write('Fishing Rod'), !
-.
-printItemName(ItemName) :-
-    ItemName == chicken_egg,
-    write('Chicken Egg'), !
-.
-printItemName(ItemName) :-
-    ItemName == milk,
-    write('Milk'), !
-.
-printItemName(ItemName) :-
-    ItemName == wool,
-    write('Wool'), !
-.
-printItemName(ItemName) :-
-    ItemName == chicken,
-    write('Chicken'), !
-.
-printItemName(ItemName) :-
-    ItemName == cow,
-    write('Cow'), !
-.
-printItemName(ItemName) :-
-    ItemName == sheep,
-    write('Sheep'), !
-.
-printItemName(ItemName) :-
-    ItemName == milk_pail_1,
-    write('Milk Pail'), !
-.
-printItemName(ItemName) :-
-    ItemName == milk_pail_2,
-    write('Milk Pail'), !
-.
-printItemName(ItemName) :-
-    ItemName == shears_1,
-    write('Shears'), !
-.
-printItemName(ItemName) :-
-    ItemName == shears_2,
-    write('Shears'), !
-.
+    write('You just drop '), write(Qty), write(' '), printItem(ItemName), write('!'), nl, !.
