9f88e4b1a794d8d7b6f4bb1fa69e5e9cafa925eb
Nelsen Putra <16520272@std.stei.itb.ac.id>
2021-11-29 15:12:43 +0700

    Rewrite quest.pl

--- quest.pl
+++ quest.pl
@@ -1,31 +1,61 @@
+/* Include modul lain */
+:- include('map.pl').
+:- include('inventory.pl').
+
 /* FACTS */
 /* questInfo(X, Y, Z): mengumpulkan X item hasil panen, Y ikan, dan Z item hasil ranching */
 :- dynamic(questInfo/3).
-/* questRemaining(A, B, C, D): A, B, C, D menggambarkan 4 quest yang ada di map, bernilai satu jika quest nya masih ada */
-:- dynamic(questRemaining/4).
-:- dynamic(inQuest/0).
 
-/* contoh questInfo */
-
-questInfo(4,3,2).
 
 /* RULES */
+quest :-
+    \+ gameStarted, !,
+    write('You need to start the game first by entering \'start.\' before you can take any quests '), nl.
 
 quest :-
-    \+ gameStarted,!,
-    write('You need to start the game first by entering \'start.\' before you can take any quests '),nl.
+    \+ isOnQuest('true'), !,
+    write('Please go to the tile \'Q\' first'), nl.
 
-quest :-
-    /* cek posisi player udah di Q ato belom*/
-    !,write('Please go to tile \'Q\' first'),nl.
+quest:-
+    isOnQuest('true'),
 
-quest :- 
-    /* game udah dimulai dan player udah di Q */
-    assertz(inQuest),!,
-    /* kayaknya harus ada id quest gitu sen biar tau ngambil quest yang mana
-    soalnya klo gini jadinya jumlah angkanya ga sesuai gitu*/
-    questInfo(A,B,C),!,
-    write('You got a new quest!'),nl,
-    format('- ~d Harvest Item',[A]),nl,
-    format('- ~d Fish Item',[A]),nl,
-    format('- ~d Ranch Item',[A]),nl.
+    /* Mengacak jumlah item quest yang harus dikumpulkan */
+    questInfo(X, Y, Z),
+    random(2, 6, X),
+    random(2, 6, Y),
+    random(2, 6, Z),
+    write('Find '), write(X), write(' Tunas, '), write(Y), write(' Potato Seeds, and '), write(Z), (' Cows!'), nl,
+    write('Rewards: 1000 exp and 5000 golds'), nl,
+    write('You can go back here once you\'ve found them all to collect the reward!'), nl,
+
+    /* Mengecek jumlah item quest pada inventory */
+    amountItem(tuna, A),
+    amountItem(potato_seeds, B),
+    amountItem(cow, C),
+    write('Right now you have: '), nl,
+    write(A), write('Tuna'), nl,
+    write(B), write('Potato Seed'), nl,
+    write(C), write('Cow'), nl,
+
+    /* Memproses redeem item dan collect reward */
+    write('Do you want to redeem the reward? (y/n)'), 
+    read(Choice),
+    (
+        Choice == 'y' ->
+        (
+            A >= X, B >= Y, C >= Z ->
+            addExp(1000),
+            addGold(5000),
+            dropItems(tuna, X),
+            dropItems(potato, Y),
+            dropItems(large_egg, Z),
+            write('Congratulations! You have completed the quest!'), nl,
+            (write('You got 1000 exp and 5000 golds'), nl);
+
+            (write('You don\'t have enough items to redeem! Try again next time!'), nl)
+        );
+        (
+        Choice == 'n' ->
+        write('You chose to not redeem your quest items with the rewards! Thank you for coming, see you!'),nl
+        )
+    ), !.
